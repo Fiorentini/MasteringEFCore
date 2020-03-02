a14e7e7e5ce840104b234d73c2f054e78a862fbc
Kalebi Fiorentini <kalebi.fiorentini@ibopedtm.com>
2020-03-02 16:05:01 -0300

    [MELHORIA] Gerando classes modelo automaticamente pelo EF Tools.

---
+++ MasteringEFCore.DatabaseFirst/Models/KalebiContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MasteringEFCore.DatabaseFirst.Models
+{
+    public partial class KalebiContext : DbContext
+    {
+        public KalebiContext()
+        {
+        }
+
+        public KalebiContext(DbContextOptions<KalebiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Blog> Blog { get; set; }
+        public virtual DbSet<Post> Post { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("user id=DTM_ASPNET;data source=DTMDEV;persist security info=True;initial catalog=Kalebi;password=M@dtmusr;Pooling=false;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.HasIndex(e => e.BlogId);
+
+                entity.Property(e => e.PublishedDateTime).HasColumnType("datetime");
+
+                entity.Property(e => e.Title).IsRequired();
+
+                entity.HasOne(d => d.Blog)
+                    .WithMany(p => p.Post)
+                    .HasForeignKey(d => d.BlogId);
+            });
+        }
+    }
+}
